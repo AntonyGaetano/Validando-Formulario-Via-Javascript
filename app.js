10603719d519985ebc28fcc1befa4c505456cd1a
Antony Gaetano <86926752+AntonyGaetano@users.noreply.github.com>
2021-09-09 07:07:22 -0300

    ....

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ var nome_invalido = document.getElementById('p_nome');
 var email_invalido = document.getElementById('p_email');
 var senha_invalida = document.getElementById('p_senha');
 
+var i_nome = document.getElementById("error-nome");
+var i_email = document.getElementById("error-email");
+var i_senha = document.getElementById("error-password");
 
+
+//Função para valicação do formulário
 const Validar = () =>{
   nome = document.getElementById('nome').value;
   email = document.getElementById('email').value;
@@ -12,29 +17,48 @@ const Validar = () =>{
     nome_invalido.innerHTML="Campo em branco";
     email_invalido.innerHTML="Email inválido";
     senha_invalida.innerHTML="Senha inválida";
+
+    i_nome.innerHTML="error_outline";
+    i_email.innerHTML="error_outline";
+    i_senha.innerHTML="error_outline";
   }
 
   else if(nome == ""){
       nome_invalido.innerHTML="Campo em branco";
-      document.getElementById('nome').focus();
       email_invalido.innerHTML="";
       senha_invalida.innerHTML="";
+  
+      i_nome.innerHTML="error_outline";
+      i_email.innerHTML="";
+      i_senha.innerHTML="";
+
+      document.getElementById('nome').focus();
       return false;
   }
 
   else if(email == "" || email.indexOf('@') == -1){
     email_invalido.innerHTML="Email inválido";
-    document.getElementById('email').focus();
     nome_invalido.innerHTML="";
     senha_invalida.innerHTML="";
+  
+    i_nome.innerHTML="";
+    i_email.innerHTML="error_outline";
+    i_senha.innerHTML="";
+
+    document.getElementById('email').focus();
     return false;
   } 
 
   else if(senha == "" || senha.length < 5){
-    senha_invalida.innerHTML="Senha inválida";
-     document.getElementById('senha').focus();
+     senha_invalida.innerHTML="Senha inválida";
      nome_invalido.innerHTML="";
      email_invalido.innerHTML="";
+  
+     i_nome.innerHTML="";
+     i_email.innerHTML="";
+     i_senha.innerHTML="error_outline";
+
+     document.getElementById('senha').focus();
      return false;
   } 
  
@@ -50,6 +74,8 @@ for(let i = 1; i < 6; i++){
  imagens.push("./imgs/foto"+[i]+".svg"); 
 }
 
+
+//Função para fazer o slide das imagens
 function Slide(){
 
 
